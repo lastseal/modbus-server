59f80d86cd2a2ef88584ffb9c0e417403558d2cd
Rodrigo <rarconte@lastseal.com>
2022-10-26 21:05:39 -0300

    CHG: se deja cliente modbus en loop

--- test/modbus-client.py
+++ test/modbus-client.py
@@ -12,14 +12,17 @@ import socket
 
 registers = [0, 6, 12, 18, 24, 30, 36, 42]
 
-ts = dt.now()
+while True:
+    ts = dt.now()
 
-c = ModbusClient('127.0.0.1', 5020, unit_id=2)
-c.open()
+    c = ModbusClient('127.0.0.1', 5020, unit_id=2)
+    c.open()
 
-if c.is_open:
-    for x in registers:
-        regs = c.read_holding_registers(x, 6)    # read function
-        if regs:
-            print("{:%d/%m/%Y %H:%M:%S} - {}".format(ts, regs))
-    
+    if c.is_open:
+        for x in registers:
+            regs = c.read_holding_registers(x, 6)    # read function
+            if regs:
+                print("{:%d/%m/%Y %H:%M:%S} - {}".format(ts, regs))
+
+    time.sleep(1)
+        
